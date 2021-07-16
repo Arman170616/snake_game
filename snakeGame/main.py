da070a216142e2b72966e5c8f7c117f39f4dbdec
Arman170616 <armanice9@gmail.com>
2021-07-17 01:46:18 +0600

    first commit

---
+++ snakeGame/main.py
@@ -0,0 +1,32 @@
+import pygame
+
+import time
+import random
+
+#Init pygame
+
+pygame.init()
+
+
+#Define Colours
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+orange = (255, 165, 0)
+
+width, height = 600, 400
+
+game_display = pygame.display.set_mode(width, height)
+pygame.display.set_caption("pyarena snake Game")
+
+clock = pygame.time.Clock()
+
+snake_size = 10
+
+snake_speed = 15
+
+message_font = pygame.font.SysFont('ubuntu', 30)
+
+score_font = pygame.font.SysFont('ubuntu', 25)
+
